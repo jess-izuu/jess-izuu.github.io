8e699978ac83e5f58d60e8b9333777e42fd53566
jess-izuu <izumijessica@gmail.com>
2017-04-01 17:25:51 -0400

    Update google-maps.js

--- google-maps.js
+++ google-maps.js
@@ -1,41 +1,100 @@
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 6
-        });
-        var infoWindow = new google.maps.InfoWindow({map: map});
+    var venueMap;
+function init() {
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+  var pinLocation = new google.maps.LatLng(40.782710,-73.965310);
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } 
-       else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
+  var mapOptions = {
+    zoom: 15,
+    center: pinLocation,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    panControl: false,
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.SMALL,
+      position: google.maps.ControlPosition.TOP_RIGHT
+    },
+
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      position: google.maps.ControlPosition.TOP_LEFT
+    },
+
+    scaleControl: true,
+    scaleControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER
+    },
+    streetViewControl: false,
+    overviewMapControl: false,
+
+    styles: [
+      {
+        stylers: [
+          { hue: "#00ff6f" },
+          { saturation: -50 }
+        ]
+      }, {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          { lightness: 100 },
+          { visibility: "simplified" }
+        ]
+      }, {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [
+          { hue: "#ff6600" },
+          { saturation: +80 }
+        ]
+      }, {
+        featureType: "transit",
+        elementType: "labels",
+        stylers: [
+          { hue: "#ff0066" },
+          { saturation: +80 }
+        ]
+      }, {
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [
+          { visibility: "off" }
+        ]
+      }, {
+        featureType: "poi.park",
+        elementType: "labels",
+        stylers: [
+          { visibility: "on" }
+        ]
+      }, {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [
+          { hue: "#c4f4f4" }
+        ]
+      }, {
+        featureType: "road",
+        elementType: "labels",
+        stylers: [
+          { visibility: "off" }
+        ]
       }
+    ]
+  };
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-            'Error: The Geolocation service failed.' :
-            'Error: Your browser doesn\'t support geolocation.');
+  var venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  var startPosition = new google.maps.Marker({    // Create a new marker
+    position: pinLocation,                        // Set its position
+    map: venueMap,                                // Specify the map
+    icon: "img/go.png"                            // Path to image from HTML
+  });
+
 }
 
 function loadScript() {
   var script = document.createElement('script');
-  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAxHN-4rLbOTrxBIC6F3jCn8mWO_fYjMf0&callback=initMap';
+  script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
   document.body.appendChild(script);
 }
 
